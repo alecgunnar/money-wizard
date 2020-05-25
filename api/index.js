93b6c916201c7d9ef9e99d66eef896b72b4139d4
Alec Carpenter <alecgunnar@gmail.com>
2020-05-24 20:01:24 -0400

    Initialize express app

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const serverPort = 8080
+
+app.get('/', (_, res) => {
+  res.json({
+    name: 'Money Wizard'
+  })
+})
+
+app.listen(serverPort);
+console.log(`Server running on port ${serverPort}`)
