3faf4cc676790ce6ef1323e0a9cee452f1ec418f
Alec Carpenter <alecgunnar@gmail.com>
2020-06-10 15:10:26 -0400

    Show new transaction form

--- ui/src/views/Transactions.vue
+++ ui/src/views/Transactions.vue
@@ -1,16 +1,68 @@
 <template>
   <div>
-    <button data-qa="new-transaction"></button>
-    <h1>Transactions</h1>
+    <div class="header">
+      <h1 class="header__title">Transactions</h1>
+      <div class="header__buttons">
+        <button data-qa="new-transaction"
+          class="button"
+          @click="addTransaction">Add Transaction</button>
+      </div>
+    </div>
+    <div v-if="addingTransaction">
+      <NewTransactionForm />
+    </div>
   </div>
 </template>
 
 <script>
+import NewTransactionForm from '@/components/forms/NewTransactionForm'
+
 export default {
-  name: 'transactions'
+  name: 'transactions',
+  data () {
+    return {
+      addingTransaction: false
+    }
+  },
+  methods: {
+    addTransaction () {
+      this.addingTransaction = true
+    }
+  },
+  components: {
+    NewTransactionForm
+  }
 }
 </script>
 
 <style scoped>
+.header {
+  display: flex;
+  margin: 0 0 2rem;
+}
 
+.header__title,
+.header__buttons {
+  line-height: 3rem;
+  flex: 1;
+}
+
+.header__title {
+  margin: 0;
+}
+
+.header__buttons {
+  text-align: right;
+}
+
+.button {
+  background: #4CAF50;
+  font-size: 1.5rem;
+  color: #fff;
+  border: none;
+  outline: none;
+  padding: 0.5rem 1rem;
+  border-radius: 3px;
+  cursor: pointer;
+}
 </style>
--- ui/test/unit/specs/views/Transactions.spec.js
+++ ui/test/unit/specs/views/Transactions.spec.js
@@ -1,9 +1,22 @@
 import Transactions from '@/views/Transactions'
+import NewTransactionForm from '@/components/forms/NewTransactionForm'
 import {shallowMount} from '@vue/test-utils'
 
 describe('Transactions', () => {
   it('has a button to create new transaction', () => {
     const subject = shallowMount(Transactions)
     expect(subject.find('button[data-qa=new-transaction]').exists()).toBeTruthy()
+  })
+
+  it('shows a form to create a new transaction', async () => {
+    const subject = shallowMount(Transactions)
+    subject.find('[data-qa=new-transaction]').trigger('click')
+    await subject.vm.$nextTick()
+    expect(subject.findComponent(NewTransactionForm).exists()).toBeTruthy()
+  })
+
+  it('does not show new transaction form by default', () => {
+    const subject = shallowMount(Transactions)
+    expect(subject.findComponent(NewTransactionForm).exists()).toBeFalsy()
   })
 })
