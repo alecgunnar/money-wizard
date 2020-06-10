5788d58c4cd5573213a0011c15adb08fda7c3d25
Alec Carpenter <alecgunnar@gmail.com>
2020-06-10 14:18:52 -0400

    Add summary sections

--- ui/src/components/views/Summary.vue
+++ ui/src/components/views/Summary.vue
@@ -1,5 +1,15 @@
 <template>
-  
+  <div class="summary">
+    <div class="summary__category">
+      <h2 class="summary__headline summary__headline--wants">Wants</h2>
+    </div>
+    <div class="summary__category">
+      <h2 class="summary__headline summary__headline--savings">Savings</h2>
+    </div>
+    <div class="summary__category">
+      <h2 class="summary__headline summary__headline--needs">Needs</h2>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +19,28 @@ export default {
 </script>
 
 <style scoped>
+.summary {
+  display: flex;
+}
 
+.summary__category {
+  flex: 1;
+}
+
+.summary__headline {
+  color: #fff;
+  padding: 0.5rem;
+}
+
+.summary__headline--needs {
+  background-color: #F44336;
+}
+
+.summary__headline--wants {
+  background-color: #4CAF50;
+}
+
+.summary__headline--savings {
+  background-color: #FFC107;
+}
 </style>
