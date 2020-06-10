b752fa873366a38342fd805ddac46795f6abe8b7
Alec Carpenter <alecgunnar@gmail.com>
2020-06-10 15:25:56 -0400

    Introduce transactions module

--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import state from './state'
-import getters from './getters'
-import mutations from './mutations'
-import actions from './actions'
+import transactions from './transactions'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state,
-  getters,
-  mutations,
-  actions
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    transactions
+  }
 })
