074514fbc80b9ca0e8037376a1ebbc9317113414
Alec Carpenter <alecgunnar@gmail.com>
2020-06-11 16:21:13 -0400

    Remove dummy data

--- api/index.js
+++ api/index.js
@@ -10,29 +10,11 @@ app.get('/', (_, res) => {
 })
 
 app.get('/transactions', (_, res) => {
-  res.json([
-    {
-      id: '5fd9beba-5980-47ee-95cf-87e92c2d7b48',
-      amount: 10.00,
-      reason: 'Testing',
-      date: '2020-05-01'
-    }
-  ])
+  res.json([])
 })
 
 app.get('/accounts', (_, res) => {
-  res.json([
-    {
-      id: '32f4ccf3-8d3c-4182-b0ca-8139bf0233ba',
-      name: 'Checking Account',
-      balance: 1421.45
-    },
-    {
-      id: '12263d1a-f875-4505-ab53-7c40d1991039',
-      name: 'Savings Account',
-      balance: 422532.57
-    }
-  ])
+  res.json([])
 })
 
 app.listen(serverPort, () => console.log(`Server running on port ${serverPort}`));
