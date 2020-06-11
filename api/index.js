db303c7227be1ac4f33adc64d5210b37209b1f04
Alec Carpenter <alecgunnar@gmail.com>
2020-06-10 22:01:39 -0400

    Configured proxy

--- api/index.js
+++ api/index.js
@@ -3,10 +3,26 @@ const app = express()
 
 const serverPort = 8081
 
+app.use((req, _, next) => {
+  console.log(`Request for: ${req.originalUrl}`)
+  next()
+})
+
 app.get('/', (_, res) => {
   res.json({
     name: 'Money Wizard'
   })
+})
+
+app.get('/transactions', (_, res) => {
+  res.json([
+    {
+      id: '5fd9beba-5980-47ee-95cf-87e92c2d7b48',
+      amount: 10.00,
+      reason: 'Testing',
+      date: '2020-05-01'
+    }
+  ])
 })
 
 app.listen(serverPort, () => console.log(`Server running on port ${serverPort}`));
--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,9 +2,11 @@ module.exports = {
     devServer: {
         proxy: {
             '^/api': {
-                target: 'http://localhost:8081'
+                target: 'http://localhost:8081',
+                changeOrigin: true,
+                pathRewrite: {'^/api': '/'},
+                logLevel: 'debug'
             }
         }
     }
 }
-
