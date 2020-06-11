c37e4edffd13d406aa1004def1ef20c8fc6386b4
Alec Carpenter <alecgunnar@gmail.com>
2020-06-11 08:45:57 -0400

    Add main nav
    Add accounts page

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -5,9 +5,17 @@
       data-qa="server-error-message">
       <p class="serverError__message">{{ serverError }}</p>
     </div>
-    <header>
-      <div class="wrapper">
-        Money Wizard
+    <header class="header">
+      <div class="wrapper header__wrapper">
+        <div class="branding">
+          Money Wizard
+        </div>
+        <nav class="navigation">
+          <ul class="navigations__links">
+            <li class="navigation__link"><RouterLink :to="{name: 'accounts'}">Accounts</RouterLink></li>
+            <li class="navigation__link"><RouterLink :to="{name: 'transactions'}">Transactions</RouterLink></li>
+          </ul>
+        </nav>
       </div>
     </header>
     <div class="wrapper">
@@ -26,14 +34,6 @@ export default {
 </script>
 
 <style scoped>
-  header {
-    background-color: #4CAF50;
-    color: #fff;
-    font-size: 3rem;
-    padding: 0.5rem 0;
-    margin: 0 0 2rem;
-  }
-
   .wrapper {
     width: 960px;
     margin: 0 auto;
@@ -46,5 +46,46 @@ export default {
     padding: 0.5rem;
     text-align: center;
     margin: 0;
+  }
+
+  .header {
+    background-color: #4CAF50;
+    margin: 0 0 2rem;
+  }
+
+  .header__wrapper {
+    display: flex;
+    align-items: center;
+  }
+
+  .branding {
+    color: #fff;
+    font-size: 3rem;
+    line-height: 5rem;
+    margin: 0 2rem 0 0;
+  }
+
+  .navigation {
+    flex: 1;
+  }
+
+  .navigations__links {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .navigation__link {
+    display: inline;
+  }
+
+  .navigation__link a {
+    color: #fff;
+    text-decoration: none;
+    padding: 0 0.5rem;
+  }
+
+  .navigation__link a.router-link-exact-active {
+    text-decoration: underline;
   }
 </style>
--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Transactions from '@/views/Transactions'
+import Accounts from '@/views/Accounts'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'transactions',
       component: Transactions
+    },
+    {
+      path: '/accounts',
+      name: 'accounts',
+      component: Accounts
     }
   ]
 })
