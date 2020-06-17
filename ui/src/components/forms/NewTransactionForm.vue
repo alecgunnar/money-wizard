0305ba5f3df2914f8d68eb8d39076c011f497f16
Alec Carpenter <alecgunnar@gmail.com>
2020-06-17 13:17:51 -0400

    Style the form

--- ui/src/components/forms/NewTransactionForm.vue
+++ ui/src/components/forms/NewTransactionForm.vue
@@ -1,13 +1,36 @@
 <template>
   <div>
-    <select data-qa="choose-account"
-      v-model="account">
-      <option :value="null"
-        disabled>Choose an Account</option>
-      <option v-for="account in accounts"
-        :key="account.id">{{ account.name }}</option>
-    </select>
-    <input data-qa="amount" />
+    <div class="form">
+      <div class="form__row">
+        <div class="form__label">
+          <label for="account">
+            Account
+          </label>
+        </div>
+        <div class="form__input">
+          <select data-qa="choose-account"
+            id="account"
+            v-model="account">
+            <option :value="null"
+              disabled>Choose an Account</option>
+            <option v-for="account in accounts"
+              :key="account.id">{{ account.name }}</option>
+          </select>
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__label">
+          <label for="amount">
+            Amount
+          </label>
+        </div>
+        <div class="form__input">
+          <input type="text"
+            id="amount"
+            data-qa="amount" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,3 +56,42 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.submitFailure {
+  background-color: #FFCDD2;
+  color: #D50000;
+  padding: 0.5em;
+  margin: 0 0 2em;
+}
+
+.form__row {
+  display: flex;
+  margin: 0 0 2em;
+  position: relative;
+}
+
+.form__fieldError {
+  color: #D50000;
+  position: absolute;
+}
+
+.form__label {
+  text-align: right;
+  width: 30%;
+  margin: 0 1em 0 0;
+  padding: 0.5em 0;
+}
+
+.form__input {
+  flex: 1;
+}
+
+.form__footer {
+  padding-left: calc(30% + 1em);
+
+  button {
+    margin: 0 0.5em 0 0;
+  }
+}
+</style>
