aab9fbee8233e55a7c09347100577be76cc4a3b0
Alec Carpenter <alecgunnar@gmail.com>
2020-06-23 11:09:19 -0400

    Add test for grouped transactions

--- api/test/repositories/transactions.spec.js
+++ api/test/repositories/transactions.spec.js
@@ -68,6 +68,48 @@ describe('Transactions Repository', () => {
     ])
   })
 
+  it('gets all transactions and groups them', async () => {
+    const accountId = await accountsRepo.createAccount('Sample', 'asset')
+    const firstTransactionId = await transactionsRepo.createTransaction(
+      accountId,
+      'debit',
+      10.57,
+      '2020-06-17',
+      ''
+    )
+
+    const secondTransactionid = await transactionsRepo.createTransaction(
+      accountId,
+      'debit',
+      44.02,
+      '2020-06-18',
+      ''
+    )
+
+    return expect(transactionsRepo.getGroupedTransactions()).resolves.toEqual({
+      '2020-06-17': [
+        {
+          id: firstTransactionId,
+          accountId: accountId,
+          type: 'debit',
+          amount: 10.57,
+          date: '2020-06-17',
+          notes: ''
+        }
+      ],
+      '2020-06-18': [
+        {
+          id: secondTransactionid,
+          accountId: accountId,
+          type: 'debit',
+          amount: 44.02,
+          date: '2020-06-18',
+          notes: ''
+        }
+      ]
+    })
+  })
+
   it('gets the transactions for an account', async () => {
     const accountId = await accountsRepo.createAccount('Sample', 'asset')
     const firstTransactionId = await transactionsRepo.createTransaction(
