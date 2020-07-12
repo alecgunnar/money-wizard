84ebfb30c816042174a6bac789ce90cfa3b2aac8
Alec Carpenter <alecgunnar@gmail.com>
2020-07-12 00:02:08 -0400

    Add store to add trans form tests

--- ui/tests/unit/specs/components/forms/NewTransactionForm.spec.js
+++ ui/tests/unit/specs/components/forms/NewTransactionForm.spec.js
@@ -2,7 +2,25 @@ import NewTransactionForm from '@/components/forms/NewTransactionForm'
 import AccountsClient from '@/clients/accounts'
 import TransactionsClient from '@/clients/transactions'
 import moment from 'moment'
-import {shallowMount} from '@vue/test-utils'
+import {shallowMount, createLocalVue} from '@vue/test-utils'
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+let store
+
+const setupTest = (addTr = null) => {
+  store = new Vuex.Store({
+    modules: {
+      transactions: {
+        actions: {
+          addTransaction: addTr || jest.fn()
+        }
+      }
+    }
+  })
+}
 
 jest.mock('@/clients/accounts')
 jest.mock('@/clients/transactions')
@@ -31,24 +49,44 @@ describe('NewTransactionForm', () => {
   })
 
   it('has a field to select an account', () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     expect(subject.find('select[data-qa=choose-account]').exists()).toBeTruthy()
   })
 
   it('loads accounts', () => {
-    shallowMount(NewTransactionForm)
+    setupTest()
+    
+    shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     expect(AccountsClient.getAccounts).toBeCalled()
   })
 
   it('provides an option for each account', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     const options = subject.findAll('[data-qa=choose-account] option')
     expect(options.length).toBe(4)
   })
 
   it('fills each account option with the name of the account', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     const options = subject.findAll('[data-qa=choose-account] option')
     expect(options.at(1).text()).toBe('Sample')
@@ -56,6 +94,8 @@ describe('NewTransactionForm', () => {
   })
 
   it('preselects the propped account', async () => {
+    setupTest()
+    
     const subject = shallowMount(NewTransactionForm, {
       propsData: {
         preselect: 456
@@ -66,38 +106,73 @@ describe('NewTransactionForm', () => {
   })
 
   it('has a field to enter an amount', () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     expect(subject.find('input[data-qa=amount]').exists()).toBeTruthy()
   })
 
   it('has a field to enter the date of the transaction', () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     expect(subject.find('input[data-qa=date]').exists()).toBeTruthy()
   })
 
   it.skip('the date field is pre-filled with the current date', () => {
     const todaysDate = moment().format('MM/DD/YYYY')
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     expect(subject.find('[data-qa=date]').element.value).toBe(todaysDate)
   })
 
   it('has a field to enter a readon', () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     expect(subject.find('input[data-qa=reason]').exists()).toBeTruthy()
   })
 
   it('has a field to enter notes', () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     expect(subject.find('textarea[data-qa=notes]').exists()).toBeTruthy()
   })
 
   it('there is a submit button', () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     expect(subject.find('button[type=submit][data-qa=submit]').exists()).toBeTruthy()
   })
 
   it('when an asset type account is selected, the asset transaction type fields are shown', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(123)
     await subject.vm.$nextTick()
@@ -105,7 +180,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('when a credit type account is selected, the credit transaction type fields are shown', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(456)
     await subject.vm.$nextTick()
@@ -113,7 +193,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('when a loan type account is selected, the credit transaction type fields are shown', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(789)
     await subject.vm.$nextTick()
@@ -121,7 +206,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('when a credit type account is selected, the asset transaction type fields are not shown', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(456)
     await subject.vm.$nextTick()
@@ -129,7 +219,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('when a loan type account is selected, the asset transaction type fields are not shown', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(789)
     await subject.vm.$nextTick()
@@ -137,7 +232,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('when a asset type account is selected, the credit transaction type fields are not shown', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(123)
     await subject.vm.$nextTick()
@@ -145,32 +245,57 @@ describe('NewTransactionForm', () => {
   })
 
   it('when no account is selected, the disabled types appear', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     expect(subject.find('[data-qa=choose-account-types]').exists()).toBeTruthy()
   })
 
   it('when no account is selected, the asset types do not appear', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     expect(subject.find('[data-qa=asset-types]').exists()).toBeFalsy()
   })
 
   it('when no account is selected, the credit types do not appear', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     expect(subject.find('[data-qa=credit-types]').exists()).toBeFalsy()
   })
 
   it('submiting without choosing an account results in an error', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     subject.find('[data-qa=add-transaction-form]').trigger('submit')
     await subject.vm.$nextTick()
     expect(subject.find('[data-qa=choose-account-error]').exists()).toBeTruthy()
   })
 
   it('submiting with an account does not result in an error', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(123)
     subject.find('[data-qa=add-transaction-form]').trigger('submit')
@@ -179,7 +304,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('submiting without choosing a type results in an error', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=add-transaction-form]').trigger('submit')
     await subject.vm.$nextTick()
@@ -187,7 +317,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('submiting with a type does not result in an error', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(123)
     await subject.vm.$nextTick()
@@ -198,7 +333,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('submitting with amount less than zero results in an error', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     subject.find('[data-qa=amount]').setValue('-10')
     subject.find('[data-qa=add-transaction-form]').trigger('submit')
     await subject.vm.$nextTick()
@@ -206,7 +346,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('submitting with amount equal to zero results in an error', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     subject.find('[data-qa=amount]').setValue('0')
     subject.find('[data-qa=add-transaction-form]').trigger('submit')
     await subject.vm.$nextTick()
@@ -214,7 +359,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('submitting with amount greater than zero does not result in an error', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     subject.find('[data-qa=amount]').setValue('100.53')
     subject.find('[data-qa=add-transaction-form]').trigger('submit')
     await subject.vm.$nextTick()
@@ -222,7 +372,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('submitting without a date results in an error', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     subject.find('[data-qa=date]').setValue('')
     subject.find('[data-qa=add-transaction-form]').trigger('submit')
     await subject.vm.$nextTick()
@@ -230,7 +385,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('submitting with a date does not result in an error', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     subject.find('[data-qa=date]').setValue('05/28/1994')
     subject.find('[data-qa=add-transaction-form]').trigger('submit')
     await subject.vm.$nextTick()
@@ -238,7 +398,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('submitting without a reason results in an error', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     subject.find('[data-qa=date]').setValue('')
     subject.find('[data-qa=add-transaction-form]').trigger('submit')
     await subject.vm.$nextTick()
@@ -246,7 +411,12 @@ describe('NewTransactionForm', () => {
   })
 
   it('submitting with a reason does not result in an error', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     subject.find('[data-qa=reason]').setValue('Testing123')
     subject.find('[data-qa=add-transaction-form]').trigger('submit')
     await subject.vm.$nextTick()
@@ -255,7 +425,12 @@ describe('NewTransactionForm', () => {
 
   it('submitting with valid input results in a service call', async () => {
     TransactionsClient.addTransaction.mockResolvedValueOnce()
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(123)
     await subject.vm.$nextTick()
@@ -272,7 +447,12 @@ describe('NewTransactionForm', () => {
 
   it('when the transaction creation succeeds an event is emitted', async () => {
     TransactionsClient.addTransaction.mockResolvedValueOnce()
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(123)
     await subject.vm.$nextTick()
@@ -288,7 +468,12 @@ describe('NewTransactionForm', () => {
 
   it('when the transaction creation fails an event is not emitted', async () => {
     TransactionsClient.addTransaction.mockRejectedValueOnce()
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(123)
     await subject.vm.$nextTick()
@@ -304,7 +489,12 @@ describe('NewTransactionForm', () => {
 
   it('when the transaction creation fails an error is shown', async () => {
     TransactionsClient.addTransaction.mockRejectedValueOnce()
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(123)
     await subject.vm.$nextTick()
@@ -320,7 +510,12 @@ describe('NewTransactionForm', () => {
 
   it('when the transaction creation succeeds an error is not shown', async () => {
     TransactionsClient.addTransaction.mockResolvedValueOnce()
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=choose-account]').setValue(123)
     await subject.vm.$nextTick()
@@ -335,19 +530,34 @@ describe('NewTransactionForm', () => {
   })
 
   it('submitting with invalid input does not result in a service call', async () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     await subject.vm.$nextTick()
     subject.find('[data-qa=add-transaction-form]').trigger('submit')
     expect(TransactionsClient.addTransaction).not.toBeCalled()
   })
 
   it('there is a cancel button', () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     expect(subject.find('button[type=button][data-qa=cancel]').exists()).toBeTruthy()
   })
 
   it('clicking the cancel button causes event to be emitted', () => {
-    const subject = shallowMount(NewTransactionForm)
+    setupTest()
+    
+    const subject = shallowMount(NewTransactionForm, {
+      localVue,
+      store
+    })
     subject.find('[data-qa=cancel]').trigger('click')
     expect(subject.emitted('cancel')).not.toBeUndefined()
   })
