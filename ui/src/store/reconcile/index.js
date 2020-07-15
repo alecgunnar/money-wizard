a1c4ed2457316473683141434a5cbf21c84d2963
Alec Carpenter <alecgunnar@gmail.com>
2020-07-14 20:54:05 -0400

    Load transactions

--- ui/src/store/reconcile/index.js
+++ ui/src/store/reconcile/index.js
@@ -1,25 +1,35 @@
 import RootClient from '@/clients'
 
 const state = {
-  account: null
+  account: null,
+  transactions: null
 }
 
 const mutations = {
   accountLoaded (state, account) {
     state.account = account
+  },
+  transactionsLoaded (state, transactions) {
+    state.transactions = transactions
   }
 }
 
 const actions = {
-  reconcileAccount ({commit}, id) {
+  reconcileAccount ({commit, dispatch}, id) {
     return RootClient.get(`/accounts/${id}`)
       .then(resp => resp.data)
       .then(data => commit('accountLoaded', data))
+      .then(() => dispatch('loadTransactions', id))
       .then(() => true)
       .catch(() => {
         commit('encounteredServerError', null, {root: true})
         return false
       })
+  },
+  loadTransactions ({commit}, id) {
+    return RootClient.get(`/transactions?accountId=${id}&reconciled=false`)
+      .then(resp => resp.data)
+      .then(data => commit('transactionsLoaded', data))
   }
 }
 
--- ui/tests/unit/specs/store/reconcile.spec.js
+++ ui/tests/unit/specs/store/reconcile.spec.js
@@ -15,6 +15,9 @@ describe('Reconcile Module', () => {
 
   it('requests account on init', () => {
     const subject = new Vuex.Store({
+      mutations: {
+        encounteredServerError: jest.fn()
+      },
       modules: {
         reconcile: ReconcileModule
       }
@@ -31,6 +34,9 @@ describe('Reconcile Module', () => {
 
   it('retains the account data on init', async () => {
     const subject = new Vuex.Store({
+      mutations: {
+        encounteredServerError: jest.fn()
+      },
       modules: {
         reconcile: ReconcileModule
       }
@@ -53,8 +59,17 @@ describe('Reconcile Module', () => {
 
   it('the action resolves when the account is loaded', async () => {
     const subject = new Vuex.Store({
+      mutations: {
+        encounteredServerError: jest.fn()
+      },
       modules: {
-        reconcile: ReconcileModule
+        reconcile: {
+          ...ReconcileModule,
+          actions: {
+            ...ReconcileModule.actions,
+            loadTransactions: jest.fn()
+          }
+        }
       }
     })
 
@@ -68,6 +83,36 @@ describe('Reconcile Module', () => {
     const status = await subject.dispatch('reconcile/reconcileAccount', 1234)
 
     expect(status).toBeTruthy()
+  })
+
+  it('transactions are loaded once the account has been loaded', async () => {
+    const loadTransactions = jest.fn()
+
+    const subject = new Vuex.Store({
+      mutations: {
+        encounteredServerError: jest.fn()
+      },
+      modules: {
+        reconcile: {
+          ...ReconcileModule,
+          actions: {
+            ...ReconcileModule.actions,
+            loadTransactions
+          }
+        }
+      }
+    })
+
+    RootClient.get.mockResolvedValueOnce({
+      data: {
+        name: 'Something',
+        balance: 45.23
+      }
+    })
+
+    await subject.dispatch('reconcile/reconcileAccount', 1234)
+
+    expect(loadTransactions).toBeCalledWith(expect.any(Object), 1234)
   })
 
   it('the action resolves when the account fails to load', async () => {
@@ -104,5 +149,59 @@ describe('Reconcile Module', () => {
     await subject.dispatch('reconcile/reconcileAccount', 1234)
 
     expect(encounteredServerError).toBeCalled()
+  })
+
+  it('makes a call to load transactons when actioned', () => {
+    RootClient.get.mockResolvedValueOnce([])
+    
+    const subject = new Vuex.Store({
+      mutations: {
+        encounteredServerError: jest.fn()
+      },
+      modules: {
+        reconcile: ReconcileModule
+      }
+    })
+
+    subject.dispatch('reconcile/loadTransactions', 4521)
+
+    expect(RootClient.get).toBeCalledWith('/transactions?accountId=4521&reconciled=false')
+  })
+
+  it('records the transactions that are loaded', async () => {
+    RootClient.get.mockResolvedValueOnce({
+      data: [
+        {
+          type: 'debit',
+          amount: 10
+        },
+        {
+          type: 'credit',
+          amount: 14
+        }
+      ]
+    })
+
+    const subject = new Vuex.Store({
+      mutations: {
+        encounteredServerError: jest.fn()
+      },
+      modules: {
+        reconcile: ReconcileModule
+      }
+    })
+
+    await subject.dispatch('reconcile/loadTransactions', 4521)
+
+    expect(subject.state.reconcile.transactions).toEqual([
+      {
+        type: 'debit',
+        amount: 10
+      },
+      {
+        type: 'credit',
+        amount: 14
+      }
+    ])
   })
 })
